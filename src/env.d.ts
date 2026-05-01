75be9ad7ffbf95263cc4003b91507969720b6257
d0ughb0yy <tin.lucic172@gmail.com>
2026-05-01 14:09:34 +0200

    Changed title to match the portfolio one

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface Window {
+  dataLayer: unknown[];
+  gtag: (command: string, ...args: unknown[]) => void;
+}
+
+declare const dataLayer: unknown[];
+declare function gtag(...args: unknown[]): void;
